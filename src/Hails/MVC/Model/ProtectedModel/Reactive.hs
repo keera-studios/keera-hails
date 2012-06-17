654d55e35822710a89b9aba25709376e06f68588
Ivan Perez <ivan.perez@keera.es>
2012-06-17 17:43:53 +0100

    Thread-safe models for MVC applications

---
+++ src/Hails/MVC/Model/ProtectedModel/Reactive.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+-- | Protected Reactive Fields
+-- 
+-- This module defines several classes and operations that are used to
+-- create reactive fields and to bind reactive fields in the view to
+-- reactive fields in the model.
+--
+-- FIXME: Due to the restrictions in the type classes, the current
+-- version uses Model.ProtectedModel.ProtectedModelInternals.ProtectedModel.
+
+module Hails.MVC.Model.ProtectedModel.Reactive where
+
+import Data.ReactiveValue
+import Hails.MVC.Model.ProtectedModel
+import Hails.MVC.Model.ReactiveModel hiding (onEvent)
+import Hails.MVC.Model.ReactiveModel.Events
+
+type Setter a b c = ProtectedModel b c -> a -> IO()
+type Getter a b c = ProtectedModel b c -> IO a
+
+class ReactiveField a b c d | a -> b, a -> c, a -> d where
+  events    :: a -> [ d ]
+
+onChanged :: (Event d, ReactiveField a b c d) => ProtectedModel c d -> a -> IO () -> IO ()
+onChanged pm field p = mapM_ (\e -> onEvent pm e p) (events field)
+
+class ReactiveField a b c d => ReactiveReadField a b c d where
+  getter :: a -> Getter b c d
+
+class ReactiveWriteField a b c d where
+  setter :: a -> Setter b c d
+
+class (ReactiveField a b c d, ReactiveReadField a b c d, ReactiveWriteField a b c d) => ReactiveReadWriteField a b c d where
+
+data Event c => ReactiveElement a b c = ReactiveElement
+  { reEvents :: [ c ]
+  , reSetter :: Setter a b c
+  , reGetter :: Getter a b c
+  }
+
+instance Event c => ReactiveField (ReactiveElement a b c) a b c where
+ events = reEvents
+
+instance Event c => ReactiveReadField (ReactiveElement a b c) a b c where
+ getter = reGetter
+
+instance Event c => ReactiveWriteField (ReactiveElement a b c) a b c where
+ setter = reSetter
+
+instance Event c => ReactiveReadWriteField (ReactiveElement a b c) a b c where
+
+type FieldAccessor a b c = ProtectedModel b c -> ReactiveFieldReadWrite a
+
+mkFieldAccessor :: (InitialisedEvent c, Event c) => ReactiveElement a b c -> ProtectedModel b c -> ReactiveFieldReadWrite a
+mkFieldAccessor (ReactiveElement evs setter' getter') pm = ReactiveFieldReadWrite set get notify
+  where set      = setter' pm
+        get      = getter' pm
+        notify p = mapM_ (\e -> onEvent pm e p) (initialisedEvent : evs)
---
+++ src/Hails/MVC/Model/ReactiveFields.hs
@@ -0,0 +1,21 @@
+module Hails.MVC.Model.ReactiveFields where
+
+import Hails.MVC.Model.ReactiveModel
+
+-- The following code presents a possibly simpler way of creating reactive
+-- fields in a reactive model.
+type Field a b c = (b -> a, a -> b -> Bool, a -> b -> b, c)
+
+preTrue :: a -> b -> Bool
+preTrue = const $ const True
+
+fieldSetter :: (Eq a, Event c) => 
+               Field a b c -> ReactiveModel b c d -> a -> ReactiveModel b c d
+fieldSetter f@(_, pre, rSet, ev) rm newVal
+  | fieldGetter f rm == newVal       = rm
+  | not $ pre newVal $ basicModel rm = triggerEvent rm ev
+  | otherwise                        = triggerEvent rm' ev
+ where rm' = rm `onBasicModel` rSet newVal
+
+fieldGetter :: (Event c) => Field a b c -> ReactiveModel b c d -> a
+fieldGetter (rGet,_,_,_) = rGet . basicModel
