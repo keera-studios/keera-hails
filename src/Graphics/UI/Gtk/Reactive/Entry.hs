e6cf5ed83cda82de06ff05c31c8a150facebf0a0
Ivan Perez <ivan.perez@keera.es>
2012-07-03 09:54:32 +0100

    Fixes error in entry change detection. Adds class constraint to signature.

--- src/Graphics/UI/Gtk/Reactive/Entry.hs
+++ src/Graphics/UI/Gtk/Reactive/Entry.hs
@@ -1,16 +1,12 @@
 -- | Publishes the main elements of an entry as reactive fields
 module Graphics.UI.Gtk.Reactive.Entry where
 
+import Control.Monad (void)
 import Graphics.UI.Gtk
 import Data.ReactiveValue
 
-entryTextReactive :: Entry -> ReactiveFieldReadWrite String
+entryTextReactive :: (EditableClass e, EntryClass e) => e -> ReactiveFieldReadWrite String
 entryTextReactive e = ReactiveFieldReadWrite setter getter notifier
  where getter     = get e entryText
        setter v   = set e [entryText := v]
-       notifier p = do on e entryPasteClipboard p
-                       on e entryCutClipboard p
-                       on e entryDeleteFromCursor (\_ _ -> p)
-                       on e entryInsertAtCursor (\_ -> p)
-                       on e entryPreeditChanged (\_ -> p)
-                       return ()
+       notifier p = void (on e editableChanged p)
