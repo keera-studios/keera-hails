272c8617816f204d10c5e17905436ca86810d1ac
Ivan Perez <ivan.perez@keera.co.uk>
2015-02-17 14:09:45 +0000

    Subclasses line edits and push buttons. Fixes bug in change detection for lineedit text.

--- src/Graphics/UI/Qt/Reactive.hs
+++ src/Graphics/UI/Qt/Reactive.hs
@@ -2,34 +2,57 @@
 module Graphics.UI.Qt.Reactive
    ( buttonClick
    , lineEditText
+   , ReactQLineEdit
+   , reactiveQLineEdit
+   , ReactQPushButton
+   , reactiveQPushButton
    )
   where
 
 import Control.Concurrent
 import Data.ReactiveValue
+
 import Qtc.ClassTypes.Core
 import Qtc.ClassTypes.Gui
 import Qtc.Classes.Base
 import Qtc.Classes.Qccs
 import Qtc.Core.Base
-import Qtc.Gui.QLineEdit ()
+import Qtc.Gui.QLineEdit
+import Qtc.Gui.QPushButton
+
+-- * Line edits
+
+type ReactQLineEdit = QLineEditSc CReactQLineEdit
+data CReactQLineEdit = CReactQLineEdit 
+ 
+reactiveQLineEdit :: IO ReactQLineEdit 
+reactiveQLineEdit = qSubClass $ qLineEdit () 
 
 -- | Make a line edit's text reactive. Does not fully work yet
 --   read/write work but the changes are not detected.
-lineEditText :: QLineEdit a -> IO (ReactiveFieldReadWrite IO String)
+lineEditText :: ReactQLineEdit -> IO (ReactiveFieldReadWrite IO String)
 lineEditText entry = do
   notifiers <- notifiersNew
 
-  connectSlot entry "textChanged(string)" entry "onTextChanged(string)"
-      (\(_ :: QObject a) s -> print $ s ++ "p")
+  connectSlot entry "textChanged(QString)" entry "onTextChanged(QString)"
+      (\(_ :: QObject a) (_ :: String) -> runNotifiers notifiers)
 
   let getter   = text entry ()
-      setter v = do t <- text entry ()
+      setter v = do -- Break circularity
+                    t <- text entry ()
                     when (t /= v) $ do setText entry v
                                        runNotifiers notifiers
       notifier = addNotifier notifiers
 
   return $ ReactiveFieldReadWrite setter getter notifier
+
+-- * Buttons
+
+type ReactQPushButton  = QPushButtonSc CReactQPushButton
+data CReactQPushButton = CReactQPushButton
+
+reactiveQPushButton :: String -> IO ReactQPushButton
+reactiveQPushButton t = qSubClass $ qPushButton t
 
 -- | Make a button click reactive.
 buttonClick :: QPushButtonSc a -> IO (ReactiveFieldRead IO ())
@@ -44,6 +67,8 @@ buttonClick tb = do
       notifier = addNotifier notifiers
 
   return $ ReactiveFieldRead getter notifier
+
+-- * Internal (notifier queue)
 
 -- | This is auxiliary code for a list of notifiers (event handlers).
 --   It will eventually be moved to a separate module.
