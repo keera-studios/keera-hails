a74c980ead1dc9600543f92cdf131ea4a849e579
Ivan Perez <ivan.perez@keera.co.uk>
2015-03-14 22:09:44 +0000

    Reactive Polling

---
+++ src/Hails/Polling.hs
@@ -0,0 +1,23 @@
+module Hails.Polling where
+
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever, void)
+import Data.CBMVar
+import Data.ReactiveValue
+
+pollingReactive :: IO a 
+                -> Maybe Int
+                -> IO (ReactiveFieldRead IO a)
+pollingReactive sensor delay = do
+  initialV <- sensor
+  mvar <- newCBMVar initialV
+
+  forkIO $ forever $ do v <- sensor
+                        writeCBMVar mvar v
+                        maybe (return ()) (void . threadDelay) delay
+              
+  -- RV fields
+  let getter   = readCBMVar mvar
+      notifier = installCallbackCBMVar mvar
+
+  return $ ReactiveFieldRead getter notifier
