2fe80effce7f654eb9a27959a9077b0b54393756
Ivan Perez <ivan.perez@keera.co.uk>
2015-03-15 00:26:45 +0000

    Demonstration program to show Wiimote data on a Gtk GUI

---
+++ Minimal.hs
@@ -0,0 +1,49 @@
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IfElse
+import Data.ReactiveValue
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Reactive
+import Hails.Polling
+import System.CWiid
+
+main :: IO ()
+main = do 
+   wm <- initialiseWiimote
+
+   awhen wm $ \wm' -> do
+      wiimoteRV <- pollingReactive (cwiidGetAcc wm')
+                                   (Just 10000) -- Every 10ms
+
+      -- View
+      initGUI
+      window  <- windowNew
+      hbox    <- hBoxNew True 10
+      text1   <- entryNew
+      text2   <- entryNew
+      text3   <- entryNew
+      set window [ windowDefaultWidth   := 200, windowDefaultHeight := 200
+                 , containerBorderWidth := 10,  containerChild      := hbox
+                 ]
+      boxPackStart hbox text1 PackGrow 0
+      boxPackStart hbox text2 PackGrow 0
+      boxPackStart hbox text3 PackGrow 0
+      widgetShowAll window
+
+      -- Keera Hails - Reactive Controller
+      ((show.head.unCWiidAcc)  <$> wiimoteRV) =:> entryTextReactive text1
+      ((show.(!!1).unCWiidAcc) <$> wiimoteRV) =:> entryTextReactive text2
+      ((show.(!!2).unCWiidAcc) <$> wiimoteRV) =:> entryTextReactive text3
+      onDestroy window mainQuit
+
+      mainGUI
+
+
+initialiseWiimote :: IO (Maybe CWiidWiimote)
+initialiseWiimote = do
+  putStrLn "Initializing WiiMote. Please press 1+2 to connect."
+  wm <- cwiidOpen
+  awhen wm (void . (`cwiidSetRptMode` 15)) -- Enable button reception, acc and IR
+  return wm
