ebfc7a62d79406234ae9e5340f94dc5443fc2a14
Ivan Perez <ivan.perez@keera.co.uk>
2015-03-18 14:49:15 +0000

    Uses Wiimote.hs

--- WMGui.hs
+++ WMGui.hs
@@ -2,14 +2,14 @@
 module Main where
 
 import Control.Applicative
-import Control.Monad
 import Control.Monad.IfElse
 import Data.ReactiveValue
 import Graphics.Rendering.Pango as Pango
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Reactive
 import Hails.Polling
-import System.CWiid
+
+import Wiimote
 
 main :: IO ()
 main = do 
@@ -39,9 +39,10 @@ main = do
       labelHome     <- builderGetObject builder castToLabel  "labelHome"
       labelPlus     <- builderGetObject builder castToLabel  "labelPlus"
       labelMinus    <- builderGetObject builder castToLabel  "labelMinus"
-      labelAccVal   <- builderGetObject builder castToLabel  "labelAccVal"
-      labelPitchVal <- builderGetObject builder castToLabel  "labelPitchVal"
-      labelRollVal  <- builderGetObject builder castToLabel  "labelRollVal"
+      labelAccVal   <- builderGetObject builder castToLabel  "labelValAcc"
+      labelPitchVal <- builderGetObject builder castToLabel  "labelValPitch"
+      labelRollVal  <- builderGetObject builder castToLabel  "labelValRoll"
+
 
       -- View: show
       widgetShowAll window
@@ -62,10 +63,10 @@ main = do
       ((buttonColorF.buttonMinus) <$> wiimoteRV) =:> labelBackground labelMinus
 
       -- Controller: synchronise accelerometre data
-      ((show.accValue)            <$> wiimoteRV) =:> labelText labelAccVal 
-      ((show.pitchValue)          <$> wiimoteRV) =:> labelText labelPitchVal 
-      ((show.rollValue)           <$> wiimoteRV) =:> labelText labelRollVal 
-      onDestroy window mainQuit
+      ((show.accValue)            <$> wiimoteRV) =:> labelTextReactive labelAccVal 
+      ((show.pitchValue)          <$> wiimoteRV) =:> labelTextReactive labelPitchVal 
+      ((show.rollValue)           <$> wiimoteRV) =:> labelTextReactive labelRollVal 
+      -- onDestroy window mainQuit
 
       -- Controller: main loop
       mainGUI
@@ -77,65 +78,4 @@ main = do
 buttonColorF :: Bool -> Pango.Color
 buttonColorF s = if s then green else defColor
   where green    = Pango.Color 0 65535 0
-        defColor = Pango.Color 32768 32768 32768  -- Neutral gray
-
--- * Wiimote
-
--- | This datatype overlaps with CWiidState.
---   TODO: CWiidState is incomplete.
-data Wiimote = Wiimote {
-   buttonA     :: Bool
- , buttonB     :: Bool
- , buttonMinus :: Bool
- , buttonPlus  :: Bool
- , buttonLeft  :: Bool
- , buttonRight :: Bool
- , buttonUp    :: Bool
- , buttonDown  :: Bool
- , buttonHome  :: Bool
- , button1     :: Bool
- , button2     :: Bool
- , accValue    :: Int
- , pitchValue  :: Int
- , rollValue   :: Int
-  -- ...
- }
- deriving Eq
-
--- | Poll the wiimote, refresh every field
---
--- TODO: This function belongs in Hcwiid.
-pollWiimote :: CWiidWiimote -> IO Wiimote
-pollWiimote wm = do
-  [a,p,r] <- unCWiidAcc <$> cwiidGetAcc wm
-  enabledBtns <- cwiidGetBtnState wm
-  return Wiimote { buttonA     = cwiidIsBtnPushed enabledBtns cwiidBtnA 
-                 , buttonB     = cwiidIsBtnPushed enabledBtns cwiidBtnB
-                 , buttonMinus = cwiidIsBtnPushed enabledBtns cwiidBtnMinus
-                 , buttonPlus  = cwiidIsBtnPushed enabledBtns cwiidBtnPlus
-                 , buttonLeft  = cwiidIsBtnPushed enabledBtns cwiidBtnLeft
-                 , buttonRight = cwiidIsBtnPushed enabledBtns cwiidBtnRight
-                 , buttonUp    = cwiidIsBtnPushed enabledBtns cwiidBtnUp
-                 , buttonDown  = cwiidIsBtnPushed enabledBtns cwiidBtnDown
-                 , buttonHome  = cwiidIsBtnPushed enabledBtns cwiidBtnHome
-                 , button1     = cwiidIsBtnPushed enabledBtns cwiidBtn1
-                 , button2     = cwiidIsBtnPushed enabledBtns cwiidBtn2
-                 , accValue    = a
-                 , pitchValue  = p
-                 , rollValue   = r
-                 }
-
--- | Connect to a wiimote. Enable button reception, acc and IR
-initialiseWiimote :: IO (Maybe CWiidWiimote)
-initialiseWiimote = do
-  putStrLn "Initializing WiiMote. Please press 1+2 to connect."
-  wm <- cwiidOpen
-  awhen wm (void . (`cwiidSetRptMode` 15))
-  return wm
-
--- * Keera Hails
-labelBackground :: Label -> ReactiveFieldReadWrite Pango.Color
-labelBackground lbl = ReactiveFieldReadWrite setter getter (const (return ())
- where setter x = labelSetAttributes lbl [AttrBackground 0 (-1) x)]
-       getter   = (\ls -> head [ x | AttrBackground _ _ x <- ls])
-                  <$> labelGetAttributes lbl
+        defColor = Pango.Color 32768 32768 32768
