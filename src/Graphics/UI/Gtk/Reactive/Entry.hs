4638bf265dff51b054b39e3f6f0ac3284d876f47
Ivan Perez <ivan.perez@keera.co.uk>
2015-05-05 16:19:22 +0100

    Make entry behave like its default property RV (Text)

--- src/Graphics/UI/Gtk/Reactive/Entry.hs
+++ src/Graphics/UI/Gtk/Reactive/Entry.hs
@@ -1,12 +1,26 @@
+{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 -- | Publishes the main elements of an entry as reactive fields
 module Graphics.UI.Gtk.Reactive.Entry where
 
-import Control.Monad (void)
+import Control.Monad (void, when)
 import Graphics.UI.Gtk
 import Data.ReactiveValue
 
 entryTextReactive :: (EditableClass e, EntryClass e) => e -> ReactiveFieldReadWrite IO String
 entryTextReactive e = ReactiveFieldReadWrite setter getter notifier
  where getter     = get e entryText
-       setter v   = postGUIAsync $ set e [entryText := v]
+       setter v   = postGUIAsync $ do
+                      p <- get e entryText
+                      when (p /= v) $ set e [entryText := v]
        notifier p = void (on e editableChanged p)
+
+instance ReactiveValueReadWrite Entry String IO where
+
+instance ReactiveValueRead Entry String IO where
+ reactiveValueOnCanRead = reactiveValueOnCanRead . entryTextReactive 
+ reactiveValueRead      = reactiveValueRead . entryTextReactive
+
+instance ReactiveValueWrite Entry String IO where
+ reactiveValueWrite = reactiveValueWrite . entryTextReactive
