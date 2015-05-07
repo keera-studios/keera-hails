9dc6ce91c752aa7fbd584a23446aa10ce2d0e343
Ivan Perez <ivan.perez@keera.co.uk>
2015-05-07 13:20:30 +0100

    Passive RVs. Handler-based RVs. Documentation

--- src/Graphics/UI/Gtk/Reactive/Entry.hs
+++ src/Graphics/UI/Gtk/Reactive/Entry.hs
@@ -4,17 +4,21 @@
 -- | Publishes the main elements of an entry as reactive fields
 module Graphics.UI.Gtk.Reactive.Entry where
 
-import Control.Monad (void, when)
-import Graphics.UI.Gtk
 import Data.ReactiveValue
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Reactive.Property
 
 entryTextReactive :: (EditableClass e, EntryClass e) => e -> ReactiveFieldReadWrite IO String
-entryTextReactive e = ReactiveFieldReadWrite setter getter notifier
- where getter     = get e entryText
-       setter v   = postGUIAsync $ do
-                      p <- get e entryText
-                      when (p /= v) $ set e [entryText := v]
-       notifier p = void (on e editableChanged p)
+entryTextReactive e = reactiveProperty e editableChanged entryText
+
+-- import Control.Monad (void, when)
+-- entryTextReactive :: (EditableClass e, EntryClass e) => e -> ReactiveFieldReadWrite IO String
+-- entryTextReactive e = ReactiveFieldReadWrite setter getter notifier
+--  where getter     = get e entryText
+--        setter v   = postGUIAsync $ do
+--                       p <- get e entryText
+--                       when (p /= v) $ set e [entryText := v]
+--        notifier p = void (on e editableChanged p)
 
 instance ReactiveValueReadWrite Entry String IO where
 
--- src/Graphics/UI/Gtk/Reactive/SpinButton.hs
+++ src/Graphics/UI/Gtk/Reactive/SpinButton.hs
@@ -1,12 +1,22 @@
 -- | Publishes the main elements of a toggle button
 module Graphics.UI.Gtk.Reactive.SpinButton where
 
-import Control.Monad
-import Graphics.UI.Gtk
+import Control.GFunctor
 import Data.ReactiveValue
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Reactive.Property
 
 spinButtonActiveReactive :: SpinButton -> ReactiveFieldReadWrite IO Int
-spinButtonActiveReactive e = ReactiveFieldReadWrite setter getter notifier
- where getter   = spinButtonGetValueAsInt e
-       setter   = spinButtonSetValue e . fromIntegral
-       notifier = void . (onValueSpinned e)
+spinButtonActiveReactive e =
+  double_int <$$> reactivePropertyH e onValueSpinned spinButtonValue
+ where double_int = bijection (round, fromIntegral)
+
+spinButtonAdjustmentReactive :: SpinButton -> ReactiveFieldReadWrite IO Adjustment
+spinButtonAdjustmentReactive = (`passiveProperty` spinButtonAdjustment)
+
+-- import Control.Monad
+-- spinButtonActiveReactive :: SpinButton -> ReactiveFieldReadWrite IO Int
+-- spinButtonActiveReactive e = ReactiveFieldReadWrite setter getter notifier
+--  where getter   = spinButtonGetValueAsInt e
+--        setter   = spinButtonSetValue e . fromIntegral
+--        notifier = void . (onValueSpinned e)
