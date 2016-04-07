6b193c54ee2f5ab19455c583f1b5b94041ad5d14
Ivan Perez <ivan.perez@keera.co.uk>
2016-04-07 09:23:27 +0200

    Documentation

--- keera-hails-reactivelenses/src/Data/ReactiveLens.hs
+++ keera-hails-reactivelenses/src/Data/ReactiveLens.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE Rank2Types #-}
+-- | Apply lenses onto Reactive Values, produced RVs that focus
+-- only on a part of the value.
 module Data.ReactiveLens where
 
 import Control.Lens
 import Data.ReactiveValue
 
+-- | Apply a lens to a reactive value, produced another reactive value that
+-- focuses on a part of the first.
 reactiveFromLens :: (Monad m, Functor m)
                  => ReactiveFieldReadWrite m a
                  -> Lens' a b
                  -> ReactiveFieldReadWrite m b
-reactiveFromLens (ReactiveFieldReadWrite setter getter notifier) l =
-   ReactiveFieldReadWrite setter' getter' notifier
-  where setter' v = setter.(set l v) =<< getter
+reactiveFromLens rv@(ReactiveFieldReadWrite _setter getter notifier) l =
+    ReactiveFieldReadWrite setter' getter' notifier
+  where setter' v = reactiveValueModify rv (set l v)
         getter'   = fmap (view l) getter
 
 -- I'd love to write this, but it's not possible because Haskell does not
@@ -24,10 +28,10 @@ reactiveFromLens (ReactiveFieldReadWrite setter getter notifier) l =
 -- equivalente definition: 'flip' 'reactiveFromLens'.
 (<$$$>) :: (Monad m, Functor m)
         => Lens' a b -> ReactiveFieldReadWrite m a -> ReactiveFieldReadWrite m b
-(<$$$>) l (ReactiveFieldReadWrite setter getter notifier) =
-  ReactiveFieldReadWrite setter' getter' notifier
- where setter' v = setter.(set l v) =<< getter
-       getter'   = fmap (view l) getter
+(<$$$>) l rv@(ReactiveFieldReadWrite _setter getter notifier) =
+    ReactiveFieldReadWrite setter' getter' notifier
+  where setter' v = reactiveValueModify rv (set l v)
+        getter'   = fmap (view l) getter
 
 -- I think this bit would need monadic lenses
 -- reactiveLens :: Lens' a b -> Lens' (ReactiveFieldReadWrite m a) (ReactiveFieldReadWrite m b)
