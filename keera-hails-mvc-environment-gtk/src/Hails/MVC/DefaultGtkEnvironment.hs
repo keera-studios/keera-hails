f5ad929e5ee69d2be95a7042772e436fb1220840
Myro Pavlenko <myro@keera.co.uk>
2020-04-09 19:01:36 +0000

    Document module. Refs #45.
    Document the module both at the top level, by adding some structure, and the
    function provided about this module specialization.

--- keera-hails-mvc-environment-gtk/src/Hails/MVC/DefaultGtkEnvironment.hs
+++ keera-hails-mvc-environment-gtk/src/Hails/MVC/DefaultGtkEnvironment.hs
@@ -1,12 +1,21 @@
--- | The environment that contains both the view and the model.
+-- | Environment that contains both a model and a view.
+--
+--   Combined enviroments (CEnvs) are types that combine a model and
+--   a view. They allow for easy installing of rules that keep both
+--   in sync.
 --
 -- Copyright   : (C) Keera Studios Ltd, 2013
 -- License     : BSD3
 -- Maintainer  : support@keera.co.uk
 module Hails.MVC.DefaultGtkEnvironment
-    ( view
-    , GEnv.model
+    ( -- * Combined Environments (CEnvs)
+      GEnv.CEnv
+      -- * CEnv construction
     , GEnv.createCEnv
+      -- * CEnv access
+    , view
+    , GEnv.model
+      -- * CEnv updates
     , GEnv.installCondition
     , GEnv.installConditions
     )
@@ -17,5 +26,6 @@ import qualified Hails.MVC.GenericCombinedEnvironment as GEnv
 import           Hails.MVC.Model.ReactiveModel        (Event)
 import qualified Hails.MVC.View.GtkView               as GtkView
 
+-- | Extract the GUI from a combined environement 'CEnv'.
 view :: (GtkView.GtkGUI a, Event c) => GEnv.CEnv a b c -> a
 view = GtkView.getGUI . GEnv.view
