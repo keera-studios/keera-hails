4c223b250c043c3c6162e31ba5388919a8c792fb
Myro Pavlenko <myro@keera.co.uk>
2020-05-26 13:11:34 +0000

    Version 2 of the tutorial. Refs #52.
    Small example using GHCJS and Keera Hails that demonstrates how to create two
    widgets and synchronize their contents using reactive values.
    
    Corresponds to the second half of the first post.

--- demos/keera-hails-demos-small/src/Main.hs
+++ demos/keera-hails-demos-small/src/Main.hs
@@ -1,9 +1,21 @@
-import GHCJS.DOM          (currentDocumentUnchecked)
-import GHCJS.DOM.Document (getBodyUnchecked)
-import GHCJS.DOM.Element  (setInnerHTML)
+import GHCJS.DOM                  (currentDocumentUnchecked)
+import GHCJS.DOM.Document         (createElement, getBodyUnchecked)
+import GHCJS.DOM.HTMLInputElement (HTMLInputElement (..))
+import GHCJS.DOM.Node             (appendChild_)
+import GHCJS.DOM.Types            (Element (..), uncheckedCastTo)
+
+import Data.ReactiveValue  ((=:>))
+import Hails.MVC.View.HTML (inputTextReactive)
 
 main :: IO ()
 main = do
   doc  <- currentDocumentUnchecked
   body <- getBodyUnchecked doc
-  setInnerHTML body "Hello Haskell!"
+
+  input1 <- uncheckedCastTo HTMLInputElement <$> createElement doc "input"
+  appendChild_ body input1
+
+  input2 <- uncheckedCastTo HTMLInputElement <$> createElement doc "input"
+  appendChild_ body input2
+
+  inputTextReactive input1 =:> inputTextReactive input2
