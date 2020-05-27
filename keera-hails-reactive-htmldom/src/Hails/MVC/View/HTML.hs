ba7878d4cffce1831da35d8adf717285792ce205
Myro Pavlenko <myro@keera.co.uk>
2020-05-27 16:26:20 +0000

    Use CBMVars as RVs from keera-hails-reactive-cbmvar. Refs #54.
    The last commit introduces a separate library to turn CBMVars from the
    keera-callbacks library into RVs.
    
    This commit uses those functions, removing them from the current module.

--- keera-hails-reactive-htmldom/src/Hails/MVC/View/HTML.hs
+++ keera-hails-reactive-htmldom/src/Hails/MVC/View/HTML.hs
@@ -5,45 +5,48 @@
 -- Maintainer  : support@keera.co.uk
 module Hails.MVC.View.HTML where
 
-import Control.Applicative ((<$>))
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad (void)
-import Data.Maybe (fromJust)
-import Data.CBMVar
+import Control.Applicative            ((<$>))
+import Control.Monad                  (void)
+import Control.Monad.IO.Class         (liftIO)
+import Data.CBMVar                    (newCBMVar, writeCBMVar)
+import Data.CBMVar.Reactive           (cbmvarReactiveRO, cbmvarReactiveRW)
 import Data.ReactiveValue
-import GHCJS.DOM                      (enableInspector, webViewGetDomDocument, runWebGUI)
-import GHCJS.DOM.Document             (getBody, createElement, click, getElementById)
-import GHCJS.DOM.Element              (setInnerHTML, IsElement, mouseDown, mouseMove, input)
-import GHCJS.DOM.EventM               (on, mouseClientXY)
-import GHCJS.DOM.HTMLDivElement       (castToHTMLDivElement)
+import Data.String
+import GHCJS.DOM.Element              (IsElement, setInnerHTML)
+import GHCJS.DOM.EventM               (mouseButton, mouseClientXY, on)
+import GHCJS.DOM.EventTargetClosures  (EventName, unsafeEventName)
+import GHCJS.DOM.GlobalEventHandlers  (input, mouseDown, mouseMove)
 import GHCJS.DOM.HTMLElement          (setInnerText)
 import GHCJS.DOM.HTMLInputElement     (getValue, setValue)
-import GHCJS.DOM.HTMLParagraphElement (castToHTMLParagraphElement, HTMLParagraphElement(..))
-import GHCJS.DOM.Node                 (appendChild)
-import GHCJS.DOM.Types                (HTMLInputElement(..) , castToHTMLInputElement)
-
+import GHCJS.DOM.HTMLParagraphElement (HTMLParagraphElement (..))
+import GHCJS.DOM.Types                (HTMLElement, HTMLInputElement (..),
+                                       IsGlobalEventHandlers, IsHTMLElement,
+                                       MouseEvent, toHTMLElement, toJSString)
 
 -- * GHCJS DOM Reactive API
 inputTextReactive :: HTMLInputElement -> ReactiveFieldReadWrite IO String
 inputTextReactive x = ReactiveFieldReadWrite setter getter notifier
- where setter = setValue x . Just
-       getter = fromJust <$> getValue x
+ where setter = setValue x
+       getter = getValue x
        notifier p = void $ on x input (liftIO p)
+
+htmlElementClick :: IsHTMLElement e => e -> ReactiveFieldRead IO ()
+htmlElementClick e = ReactiveFieldRead (return ()) (\p -> void $ on e' click (liftIO p))
+  where
+    e' = toHTMLElement e
+
+click :: EventName HTMLElement MouseEvent
+click = unsafeEventName (toJSString "click")
 
 paragraphTextReactive :: HTMLParagraphElement -> ReactiveFieldWrite IO String
 paragraphTextReactive x = ReactiveFieldWrite setter
- where setter = setInnerHTML x . Just
+ where setter = setInnerHTML x
 
-mouseDownReactive :: IsElement a => a -> ReactiveFieldRead IO ()
+mouseDownReactive :: (IsGlobalEventHandlers a, IsElement a) => a -> ReactiveFieldRead IO ()
 mouseDownReactive s = eventR $ \p -> void $ on s mouseDown (liftIO p)
 
-mousePosElementReactive :: IsElement e => e -> IO (ReactiveFieldRead IO (Int, Int))
+mousePosElementReactive :: (IsGlobalEventHandlers e, IsElement e) => e -> IO (ReactiveFieldRead IO (Int, Int))
 mousePosElementReactive e = do
   p <- newCBMVar (0,0)
   void $ on e mouseMove (mouseClientXY >>= \pos -> liftIO (writeCBMVar p pos))
   return (cbmvarReactiveRO p)
-
-cbmvarReactiveRO :: CBMVar a -> ReactiveFieldRead IO a
-cbmvarReactiveRO mvar = ReactiveFieldRead getter notifier
-  where getter     = readCBMVar mvar
-        notifier n = installCallbackCBMVar mvar n
